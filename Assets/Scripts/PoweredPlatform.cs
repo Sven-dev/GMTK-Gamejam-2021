91a4bdc05a68365826f8b42cd3cd3d207fea3a37
Sven Janssen <svenjanssen12321@gmail.com>
2021-06-12 18:31:20 +0200

    Implemented Powered platform

---
+++ Assets/Scripts/PoweredPlatform.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoweredPlatform : Powerable
+{
+    [SerializeField] private float Speed = 1;
+    [Space]
+    [SerializeField] private Transform Platform;
+    [Space]
+    [SerializeField] private Transform Beginning;
+    [SerializeField] private Transform End;
+
+    private bool active = false;
+
+    private void Start()
+    {
+        StartCoroutine(_Move());
+    }
+
+    public override void PowerUp()
+    {
+        active = true;
+    }
+
+    public override void PowerDown()
+    {
+        active = false;
+    }
+
+    IEnumerator _Move()
+    {
+        int sigma = 1;
+        float progress = 0;
+        while(true)
+        {
+            if (active)
+            {
+                Platform.transform.position = Vector3.Lerp(Beginning.position, End.position, progress);
+
+                progress += Time.fixedDeltaTime * Speed * sigma;
+                if (progress >= 1)
+                {
+                    sigma = -1;
+                }
+                else if (progress <= 0)
+                {
+                    sigma = 1;
+                }
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
