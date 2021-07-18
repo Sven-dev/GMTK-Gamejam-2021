da1041fea7e3c904b66cd30887f8c556383ee01d
Sven Janssen <svenjanssen12321@gmail.com>
2021-07-18 20:06:58 +0200

    Fixed a bug where the input system wouldn't get enabled on start

---
+++ Assets/InputSystem/Input.cs
@@ -0,0 +1,301 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputSystem/Input.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Input : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Input()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Input"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""40610c0f-10b1-49b6-9bc7-f63f96a890d9"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""c49751bf-db69-4b92-9362-eeb129819d7e"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""ec6aed9c-7e71-44e4-aa20-7cb2f70f8aa8"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Interact"",
+                    ""type"": ""Button"",
+                    ""id"": ""49e6611c-f8cc-4dab-b3f2-193408a019b6"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""aef48a4b-9f69-426a-96ff-cbf705ae7d4c"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""1b0cf0c1-7f53-402a-90f8-bf2c62183716"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""235c9c67-8a6f-4b62-8287-3e291acb5d25"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""7f063f82-e209-4c18-8cfb-3b88f4e15996"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""3e8781bf-3f7f-4756-be38-a1ff956f3782"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1c921f1b-ea66-473c-9f12-2a77c9a62c83"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c2153d54-2c42-4ab1-9545-c70bd256d1e5"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Camera"",
+            ""id"": ""3cf3753e-6811-46a9-a71c-4946c447e94d"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""63513acd-e911-47bd-b5e6-2524a4d131ed"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""2ab54dc7-8f95-4436-85e8-4e55cf57117d"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Interact = m_Player.FindAction("Interact", throwIfNotFound: true);
+        // Camera
+        m_Camera = asset.FindActionMap("Camera", throwIfNotFound: true);
+        m_Camera_Move = m_Camera.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Interact;
+    public struct PlayerActions
+    {
+        private @Input m_Wrapper;
+        public PlayerActions(@Input wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Interact => m_Wrapper.m_Player_Interact;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Interact.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteract;
+                @Interact.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteract;
+                @Interact.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteract;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Interact.started += instance.OnInteract;
+                @Interact.performed += instance.OnInteract;
+                @Interact.canceled += instance.OnInteract;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+
+    // Camera
+    private readonly InputActionMap m_Camera;
+    private ICameraActions m_CameraActionsCallbackInterface;
+    private readonly InputAction m_Camera_Move;
+    public struct CameraActions
+    {
+        private @Input m_Wrapper;
+        public CameraActions(@Input wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Camera_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Camera; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CameraActions set) { return set.Get(); }
+        public void SetCallbacks(ICameraActions instance)
+        {
+            if (m_Wrapper.m_CameraActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_CameraActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_CameraActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_CameraActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_CameraActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public CameraActions @Camera => new CameraActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnInteract(InputAction.CallbackContext context);
+    }
+    public interface ICameraActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,16 +8,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Jumper Jumper;
     [SerializeField] private Interacter Interacter;
     [SerializeField] private CameraController CameraController;
+    [Space][Range(0.00f, 1.00f)]
+    [SerializeField] private float Deadzone = 0.25f;
 
     public ControlState State = ControlState.Moving;
-    private GameplayControls inputControls;
+    private Input Input;
 
     /// <summary>
     /// Create the controller and set up the instance
     /// </summary>
     private void Awake()
     {
-        inputControls = new GameplayControls();
+        Input = new Input();
         if (Instance == null) Instance = this;
         else Destroy(this);
     }
@@ -27,16 +29,19 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Start()
     {
-        if (inputControls != null)
+        Input.Enable();
+        if (Input != null)
         {
-            inputControls.Player.Jump.performed += _ => Jump();
+            Input.Player.Jump.performed += _ => Jump();
 
-            inputControls.Player.Interact.started += _ => StartInteract();
-            inputControls.Player.Interact.canceled += _ => StopInteract();
-
-            inputControls.Player.Pause.performed += _ => Pause();
-            inputControls.Player.Menu.performed += _ => OpenMenu();
+            Input.Player.Interact.started += _ => StartInteract();
+            Input.Player.Interact.canceled += _ => StopInteract();
         }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this) Instance = null;
     }
 
     /// <summary>
@@ -45,10 +50,10 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         //Get the input from the mouseposition or the right stick, and send it to the cameracontroller
-        CameraController.RotateInput = inputControls.Player.Camera.ReadValue<Vector2>();
+        CameraController.RotateInput = Input.Camera.Move.ReadValue<Vector2>();
 
         //Get the input from WASD or the left stick
-        Vector3 input = inputControls.Player.Move.ReadValue<Vector2>();
+        Vector3 input = Input.Player.Move.ReadValue<Vector2>();
         switch (State)
         {
             case ControlState.Moving:
@@ -70,8 +75,30 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void Walk(Vector3 input)
     {
-        input = Quaternion.AngleAxis(CameraController.GetHorizontalRotation(), Vector3.back) * input;
-        Mover.Input = input;
+        if (input.sqrMagnitude > Deadzone)
+        {
+            input = Quaternion.AngleAxis(CameraController.GetHorizontalRotation(), Vector3.back) * input;
+            Mover.Input = input;
+        }
+        else
+        {
+            Mover.Input = Vector3.zero;
+        }
+    }
+
+    /// <summary>
+    /// Send the input to the interactable object
+    /// </summary>
+    private void Interact(Vector2 input)
+    {
+        if (input.sqrMagnitude > Deadzone)
+        {
+            Interacter.Input = input;
+        }
+        else
+        {
+            Interacter.Input = Vector2.zero;
+        }
     }
 
     private void Jump()
@@ -82,20 +109,13 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Send the input to the interactable object
-    /// </summary>
-    private void Interact(Vector3 input)
-    {
-        Interacter.Input = input;
-    }
-
     private void StartInteract()
     {
         if (Interacter.Interactable())
         {
             State = ControlState.Interacting;
-            Interacter.Interact();
+            Interacter.StartInteract();
+            Mover.Input = Vector3.zero;
         }
     }
 
@@ -104,24 +124,9 @@ public class PlayerController : MonoBehaviour
         if (Interacter.Interactable())
         {
             State = ControlState.Moving;
+            Interacter.StopInteract();
         }
     }
-
-    private void OnDestroy()
-    {
-        if (Instance == this) Instance = null;
-    }
-
-    #region UI Related
-    private void Pause()
-    {
-        Debug.Log("You Paused");
-    }
-    private void OpenMenu()
-    {
-        Debug.Log("You want a Menu");
-    }
-    #endregion
 }
 
 public enum ControlState
