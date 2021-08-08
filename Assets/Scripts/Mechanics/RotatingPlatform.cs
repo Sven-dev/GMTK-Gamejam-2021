139105fd74611c42bef68c7217d1d8e5e6ecdf75
Sven Janssen <svenjanssen12321@gmail.com>
2021-08-08 16:08:41 +0200

    Tweaked rotating platform to rotate in 90 degree increments

--- Assets/Scripts/Mechanics/RotatingPlatform.cs
+++ Assets/Scripts/Mechanics/RotatingPlatform.cs
@@ -7,16 +7,28 @@ public class RotatingPlatform : MonoBehaviour
     [SerializeField] private float Speed;
     [SerializeField] private float Direction;
 
-    void FixedUpdate()
+    private void Start()
     {
-        transform.Rotate(Vector3.up * Direction * Speed * Time.fixedDeltaTime);
+        StartCoroutine(_Rotate());
+    }
 
-        /*
-        foreach (Transform item in transform)
+    private IEnumerator _Rotate()
+    {
+        while (true)
         {
-            item.Translate(Vector3.up * Direction * Speed * Time.fixedDeltaTime, transform);
+            yield return new WaitForSeconds(5);
+
+            Quaternion start = transform.rotation;
+            Quaternion end = Quaternion.Euler(transform.eulerAngles + Direction * Vector3.up * 90);
+
+            float progress = 0;
+            while (progress < 1)
+            {
+                transform.rotation = Quaternion.Lerp(start, end, progress);
+                progress += Speed * Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+            }
         }
-        */
     }
 
     private void OnTriggerEnter(Collider other)
